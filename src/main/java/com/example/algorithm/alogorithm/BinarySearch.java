bffa30d59f33768c5d1b0f46f6f412e3342fb40a
fuqiang <18251359382@163.com>
2020-01-16 20:21:52 +0800

    add method

--- src/main/java/com/example/algorithm/alogorithm/BinarySearch.java
+++ src/main/java/com/example/algorithm/alogorithm/BinarySearch.java
@@ -31,6 +31,35 @@ public class BinarySearch {
     }
 
     /**
+     * 查找第一个等于值的元素
+     *
+     * @param nums
+     * @param n
+     * @param value
+     * @return
+     */
+    public int searchFirstValue(int[] nums, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) >> 1;
+            if (nums[mid] > value) {
+                high = mid - 1;
+            } else if (nums[mid] < value) {
+                low = mid + 1;
+            } else {
+                if (mid == 0 || nums[mid - 1] < value) {
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+    /**
      * 查找第一个大于等于值的元素
      *
      * @param nums
