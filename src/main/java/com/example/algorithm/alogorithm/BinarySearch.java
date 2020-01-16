c879c72c75ceb8ac6dc993aecf185aaf82aa30f0
fuqiang <18251359382@163.com>
2020-01-16 21:46:19 +0800

    add method

--- src/main/java/com/example/algorithm/alogorithm/BinarySearch.java
+++ src/main/java/com/example/algorithm/alogorithm/BinarySearch.java
@@ -110,4 +110,30 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    /**
+     * 查找最后一个小于等于值的元素位置
+     *
+     * @param nums
+     * @param n
+     * @param value
+     * @return
+     */
+    public int searchLastMin(int[] nums, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) >> 1;
+            if (nums[mid] > value) {
+                high = mid - 1;
+            } else {
+                if (mid == n - 1 || nums[mid + 1] > value) {
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
 }
