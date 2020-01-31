d84ebae13f1e5dbd4833e7cd53ce6d8182777fae
fuqiang <18251359382@163.com>
2020-01-31 19:43:29 +0800

    leetcode

---
+++ src/main/java/com/example/leetcode/Num215.java
@@ -0,0 +1,27 @@
+/*
+ * @Copyright: 2018-2019 liuxing All rights reserved.
+ */
+package com.example.leetcode;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author liuxing
+ * @version Num215, v0.1 2020-01-31 19:26
+ */
+public class Num215 {
+
+    public int findKthLargest(int[] nums, int k) {
+        //大顶堆
+        PriorityQueue<Integer> heap =
+                new PriorityQueue<Integer>((n1, n2) -> n1 - n2);
+
+        for (int n : nums) {
+            heap.add(n);
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+        return heap.poll();
+    }
+}
