22dff126a552542fe812bfee9e08280f97013499
fuqiang <18251359382@163.com>
2020-04-22 16:56:28 +0800

    feat:LinkedList

---
+++ src/main/java/com/example/dataStructure/LinkedList.java
@@ -0,0 +1,136 @@
+package com.example.dataStructure;
+
+import sun.lwawt.macosx.CImage;
+
+import javax.annotation.Resource;
+
+/**
+ * @author fuqiang
+ * @version LinkedList, v0.1 2020/4/22 10:35 上午
+ */
+public class LinkedList<E> {
+
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node dummyHead;
+    private int size;
+
+    public LinkedList() {
+        dummyHead = new Node(null, null);
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. index error");
+        }
+        Node pre = dummyHead;
+        for (int i = 0; i < index; i++) {
+            pre = pre.next;
+        }
+        pre.next = new Node(e, pre.next);
+        size++;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed. index error");
+        }
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        return cur.e;
+    }
+
+    public E getFirst() {
+        return get(0);
+    }
+
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    public void set(int index, E e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Set failed. index error");
+        }
+        Node cur = dummyHead.next;
+        for (int i = 0; i < size; i++) {
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+
+    public boolean contains(E e) {
+        Node cur = dummyHead.next;
+        //可以使用while
+        for (int i = 0; i < size; i++) {
+            if (cur.e.equals(e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            sb.append(cur).append("->");
+            cur = cur.next;
+        }
+        sb.append("NULL");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+        linkedList.add(2, 666);
+        System.out.println(linkedList);
+
+    }
+}
---
+++ src/main/java/com/example/leetcode/Num37.java
@@ -0,0 +1,10 @@
+package com.example.leetcode;
+
+/**
+ * @author fuqiang
+ * @version Num37, v0.1 2020/4/21 6:13 下午
+ */
+public class Num37 {
+
+
+}
