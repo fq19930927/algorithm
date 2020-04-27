d6ec0a30cbffff9c8ac2e94867a11bfe61cf2d72
fuqiang <18251359382@163.com>
2020-04-27 16:18:29 +0800

    feat:BST

---
+++ src/main/java/com/example/dataStructure/BST.java
@@ -0,0 +1,35 @@
+package com.example.dataStructure;
+
+/**
+ * @author fuqiang
+ * @version BST, v0.1 2020/4/27 4:15 下午
+ */
+public class BST<E extends Comparable<E>> {
+
+    private class Node {
+        public E e;
+        public Node left, right;
+
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public BST() {
+        root = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
