4967a5eb3e778d3199f84ae20627efa7b76d653e
fuqiang <18251359382@163.com>
2020-05-16 16:51:15 +0800

    feat: add PriorityQueue.java

---
+++ src/main/java/com/example/dataStructure/PriorityQueue.java
@@ -0,0 +1,42 @@
+/*
+ * @Copyright: 2018-2019 liuxing All rights reserved.
+ */
+package com.example.dataStructure;
+
+/**
+ * @author liuxing
+ * @version PriorityQueue, v0.1 2020/5/16 4:47 下午
+ */
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+
+    private MaxHeap<E> maxHeap;
+
+    public PriorityQueue() {
+        maxHeap = new MaxHeap<>();
+    }
+
+    @Override
+    public int getSize() {
+        return maxHeap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return maxHeap.isEmpty();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        maxHeap.add(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return maxHeap.extractMax();
+    }
+
+    @Override
+    public E getFront() {
+        return maxHeap.findMax();
+    }
+}
