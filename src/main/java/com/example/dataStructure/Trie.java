5bd6ba86355e920b320ecae3961678ff2bf6af3b
fuqiang <18251359382@163.com>
2020-05-30 21:34:12 +0800

    feat:add Trie

---
+++ src/main/java/com/example/dataStructure/Trie.java
@@ -0,0 +1,53 @@
+package com.example.dataStructure;
+
+import java.util.TreeMap;
+
+/**
+ * @author fuqiang
+ * @version Trie, v0.1 2020/5/30 9:24 下午
+ */
+public class Trie {
+
+    private class Node {
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+    private Node root;
+    //Trie中存储的单词数量
+    private int size;
+
+    public Trie() {
+        root = new Node();
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    // 向Trie添加一个新单词word
+    public void add(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        if (!cur.isWord) {
+            cur.isWord = true;
+            size++;
+        }
+    }
+}
